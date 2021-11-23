afec0c9b62ae0928445b48e1686be1b3e6d9be7d
bernd <b.steinacher@htlstp.at>
2021-11-23 10:19:32 +0100

    create 404 page without localization

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Typography from '@mui/material/Typography';
 
 export default function NotFound() {
-  return (
-    <Layout>
-      <div>Page doesn't exist</div>
-    </Layout>
-  )
+    return (
+        <Layout>
+
+            <div align={'center'} style={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
+                <Typography fontWeight={350} color={'orange'} variant="h1" component="div" gutterBottom>
+                    404
+                </Typography>
+                <Typography variant="h4" gutterBottom component="div">
+                    UPS! NICHTS WURDE GEFUNDEN!
+                </Typography>
+                <Typography variant="body2" gutterBottom component="div">
+                    Die Seite nach der Sie suchen existiert nicht oder ist vorübergehend nicht erreichbar!
+                </Typography>
+            </div>
+        </Layout>
+    )
 }
