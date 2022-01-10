7837eda9b3c945f2e479948180e9ca23961f40f1
bernd1337htl <b.steinacher@htlstp.at>
2022-01-10 15:49:38 +0100

    add 500 page and translations

---
+++ src/pages/500.js
@@ -0,0 +1,49 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Typography from '@mui/material/Typography';
+
+import { graphql } from 'gatsby'
+import {useTranslation} from "gatsby-plugin-react-i18next";
+
+export default function ServerError() {
+    const {t} = useTranslation();
+
+    return (
+        <Layout>
+            <div>
+                <Typography style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'}} fontWeight={350} color={'orange'} variant="h1" component="div" gutterBottom>
+                    500
+                </Typography>
+                <Typography style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'}} variant="h4" gutterBottom component="div">
+                    {t("nothingFound")}
+                </Typography>
+                <Typography style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'}} variant="body2" gutterBottom component="div">
+                    {t("nothingFoundText")}
+                </Typography>
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
