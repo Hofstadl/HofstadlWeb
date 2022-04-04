40e99d6a6be3e382d9695f9db1a31b6a283a993d
ThePaulo1 <paul0802@icloud.com>
2022-04-04 16:14:08 +0200

    Add mini SEO

---
+++ src/components/SEO.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+export default function SEO() {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+      <title>Hofstadl Liebhart - Waldviertel</title>
+    </Helmet>
+  );
+}
