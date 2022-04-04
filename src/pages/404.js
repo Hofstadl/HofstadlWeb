223c6bddb24400a8c93c95d5e4153f50a84b3dd7
silvia0077 <s.silberbauer@htlstp.at>
2022-04-04 15:21:41 +0200

    Add 404 and 500 Error Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React from "react"
+import Layout from "../components/Layout"
+import ErrorPage from "../components/ErrorPage"
+
+import { graphql } from 'gatsby'
+import {useTranslation} from "gatsby-plugin-react-i18next";
 
 export default function NotFound() {
-  return (
-      <div>Page doesn't exist</div>
-  );
+    const {t} = useTranslation();
+
+    return (
+        <>
+            <ErrorPage errorCode={404} error={t("nothingFound")} errorDetails={t("nothingFoundText")}/>
+        </>
+    )
 }
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
