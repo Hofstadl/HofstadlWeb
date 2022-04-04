0da67f2d3742c32720a59fe408485d7f52f981a0
bernd <b.steinacher@htlstp.at>
2022-04-04 16:13:19 +0200

    update Activity component

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,17 +1,28 @@
 import React from "react";
 import {useTranslation} from "gatsby-plugin-react-i18next";
 import {graphql} from "gatsby";
+import {BackgroundImage, Center, Text, Box} from '@mantine/core';
 
 export default function Activity({image, headline, text}) {
     const {t} = useTranslation();
 
     return (
         <div>
-            <div className="bg-red-100 relative">
-                <img className="bg-cover bg-center" src={image} alt={t(headline)}></img>
-                <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 text-white">
-                    <div className="flex justify-center">{t(headline)}</div><br/>
-                    <div className="flex justify-center">{t(text)}</div>
+            <div>
+                <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover">
+                    <img src={image}
+                         className="w-full transition duration-300 ease-linear align-middle"/>
+                        <div className="opacity-30 hover:opacity-100 absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed">
+                            <div className="flex justify-center h-full">
+                                <h5 className="text-lg font-bold text-white m-6">{headline}</h5>
+                                <p>{t(text)}</p>
+                            </div>
+                        </div>
+                        <div className="hover-overlay">
+                            <div
+                                className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"
+                               ></div>
+                        </div>
                 </div>
             </div>
         </div>
